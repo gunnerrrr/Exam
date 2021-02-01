d3cca4037f74066655b96e9f434daeedfb679ed4
gunnerrrr <max.trakalo@gmail.com>
2021-02-01 23:53:13 +0200

    modify

--- src/main/java/second_ticket/tasks/threads_17/Main.java
+++ src/main/java/second_ticket/tasks/threads_17/Main.java
@@ -1,44 +1,50 @@
 package second_ticket.tasks.threads_17;
 
 import java.io.IOException;
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        final int size =10;
-        double [][]matrix= new double[size][size];
-        for(int i=0;i<size;i++) {
-            for(int j=0;j<size;j++) {
-                matrix[i][j]= Math.random()*200 - 100;
-                System.out.println(matrix[i][j]);
+        final int size = 10;
+        double[][] matrix = new double[size][size];
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = ThreadLocalRandom.current().nextInt(-1, 5+1);
+                System.out.print(matrix[i][j]+"      \t");
             }
         }
-         new Thread(()-> {
-                 double firstSum=0;
-                 while (!Thread.currentThread().isInterrupted()) {
-                         for (int i = 0; i < size; i++) {
-                             if (matrix[i][i] >= 0)
-                                 firstSum += matrix[i][i];
-                             else {
 
-                                 System.out.println("position is [" + i + "][" + i + "]");
-                                 Thread.currentThread().interrupt();
-                             }
-                         }
-                     }
-             System.out.println(firstSum);
+        new Thread(() -> {
+            double firstSum = 0;
+            for (int i = 0; i < size; i++) {
+                if (!Thread.currentThread().isInterrupted()) {
+                    if (matrix[i][i] >= 0)
+                        firstSum += matrix[i][i];
+                    else {
+                        System.out.println("position is [" + i + "][" + i + "]");
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+            System.out.println(firstSum);
         }).start();
 
 
-        new Thread(()-> {
-            double secondSum=0;
-            for(int i=0;i<size;i++) {
-                if(matrix[i][size-i-1]>=0)
-                    secondSum+=matrix[i][size-i-1];
-                else {
-//                    System.out.println("position is ["+i+"]["+i+"]");
-                    Thread.currentThread().interrupt();
+        new Thread(() -> {
+            double secondSum = 0;
+
+            for (int i = 0; i < size; i++) {
+                if (!Thread.currentThread().isInterrupted()) {
+                    if (matrix[i][size - i - 1] >= 0)
+                        secondSum += matrix[i][size - i - 1];
+                    else {
+                        System.out.println("position is [" + i + "][" + (size - i - 1) + "]");
+                        Thread.currentThread().interrupt();
+                    }
                 }
             }
+            System.out.println(secondSum);
         }).start();
 
     }
--- src/main/java/third_ticket/stream_16/Main.java
+++ src/main/java/third_ticket/stream_16/Main.java
@@ -5,7 +5,9 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         String [] arr= {"OHello", "My","Friend", "Darkness","Old", "My"};
-        long count= Arrays.stream(arr).filter(p->p.toLowerCase().charAt(0)==p.toLowerCase().charAt(p.length()-1)).count();
+        long count= Arrays.stream(arr)
+                .filter(p->p.toLowerCase().charAt(0)==p.toLowerCase().charAt(p.length()-1))
+                .count();
         System.out.println(count);
     }
 }
